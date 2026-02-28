1198264b8245203b9be00efebf91282d41e93939
Heisenberg976 <nikamiqeltadze159@gmail.com>
2026-02-28 23:40:09 +0400

    Switch to H2 database

--- src/main/java/com/gym/crm/Main.java
+++ src/main/java/com/gym/crm/Main.java
@@ -9,10 +9,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        GymFacade facade = context.getBean(GymFacade.class);
-
-        Trainer trainer = new Trainer();
     }
 }
 
--- src/main/java/com/gym/crm/config/AppConfig.java
+++ src/main/java/com/gym/crm/config/AppConfig.java
@@ -1,44 +1,74 @@
 package com.gym.crm.config;
 
-import com.gym.crm.model.Trainee;
-import com.gym.crm.model.Trainer;
-import com.gym.crm.model.Training;
-import com.gym.crm.model.TrainingType;
+import jakarta.persistence.EntityManagerFactory;
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.Map;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan("com.gym.crm")
 @PropertySource("classpath:application.properties")
+@EnableTransactionManagement
 public class AppConfig {
-    @Bean
-    public Map<Long, Trainer> trainerStorage() {
-        return new java.util.HashMap<>();
-    }
+    private Environment env;
 
-    @Bean
-    public Map<Long, Trainee> traineeStorage() {
-        return new java.util.HashMap<>();
-    }
-
-    @Bean
-    public Map<Long, Training> trainingStorage() {
-        return new java.util.HashMap<>();
-    }
-
-    @Bean
-    public Map<Long, TrainingType> trainingTypeStorage() {
-        return new java.util.HashMap<>();
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
     }
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
+
+    @Bean
+    public DataSource dataSource() {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL(env.getRequiredProperty("jdbc.url"));
+        ds.setUser(env.getRequiredProperty("jdbc.username"));
+        ds.setPassword(env.getRequiredProperty("jdbc.password"));
+        return ds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setPackagesToScan("com.gym.crm.model");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        factory.setJpaVendorAdapter(vendorAdapter);
+
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+
+        factory.setJpaProperties(properties);
+
+        return factory;
+    }
+
+    @Bean
+    PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
 }
 
