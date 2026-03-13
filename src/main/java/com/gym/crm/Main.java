51bc4e4490338b441e40dbdaae5c6ed757c30ff3
Heisenberg976 <nikamiqeltadze159@gmail.com>
2026-03-14 03:11:46 +0400

    Add Tomcat setup for Swagger

--- src/main/java/com/gym/crm/Main.java
+++ src/main/java/com/gym/crm/Main.java
@@ -1,15 +1,33 @@
 package com.gym.crm;
 
-import com.gym.crm.config.AppConfig;
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.gym.crm.config.WebConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.File;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        context.getBean(EntityManagerFactory.class);
-        Thread.sleep(300000); // Sleep for 5 minutes
+    public static void main(String[] args) throws InterruptedException, LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.scan("com.gym.crm");
+
+        DispatcherServlet dispatcher = new DispatcherServlet(webContext);
+
+        Context ctx = tomcat.addContext("", new File(".").getAbsolutePath());
+        Wrapper servlet = Tomcat.addServlet(ctx, "dispatcher", dispatcher);
+        servlet.setLoadOnStartup(1);
+        ctx.addServletMappingDecoded("/", "dispatcher");
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
 
